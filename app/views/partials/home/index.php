8d817a6900e1dd83946b3569b602a0de28bc1c2f
yolandaagnetaa <yolandaagneta@gmail.com>
2023-12-05 16:24:14 +0700

    first commit

---
+++ app/views/partials/home/index.php
@@ -0,0 +1,130 @@
+<?php 
+$page_id = null;
+$comp_model = new SharedController;
+$current_page = $this->set_current_page_link();
+?>
+<div>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12 comp-grid">
+                    <h4 >The Dashboard</h4>
+                </div>
+                <div class="col-md-6 comp-grid">
+                    <div class="card card-body">
+                        <?php 
+                        $chartdata = $comp_model->barchart_lababulanini();
+                        ?>
+                        <div>
+                            <h4>Laba Bulan Ini</h4>
+                            <small class="text-muted"></small>
+                        </div>
+                        <hr />
+                        <canvas id="barchart_lababulanini"></canvas>
+                        <script>
+                            $(function (){
+                            var chartData = {
+                            labels : <?php echo json_encode($chartdata['labels']); ?>,
+                            datasets : [
+                            {
+                            label: 'Keuntungan',
+                            backgroundColor:'rgba(255 , 128 , 0, 0.5)',
+                            type:'',
+                            borderWidth:3,
+                            data : <?php echo json_encode($chartdata['datasets'][0]); ?>,
+                            }
+                            ]
+                            }
+                            var ctx = document.getElementById('barchart_lababulanini');
+                            var chart = new Chart(ctx, {
+                            type:'bar',
+                            data: chartData,
+                            options: {
+                            scaleStartValue: 0,
+                            responsive: true,
+                            scales: {
+                            xAxes: [{
+                            ticks:{display: true},
+                            gridLines:{display: true},
+                            categoryPercentage: 1.0,
+                            barPercentage: 0.8,
+                            scaleLabel: {
+                            display: true,
+                            labelString: ""
+                            },
+                            }],
+                            yAxes: [{
+                            ticks: {
+                            beginAtZero: true,
+                            display: true
+                            },
+                            scaleLabel: {
+                            display: true,
+                            labelString: ""
+                            }
+                            }]
+                            },
+                            }
+                            ,
+                            })});
+                        </script>
+                    </div>
+                </div>
+                <div class="col-md-6 comp-grid">
+                    <div class="card card-body">
+                        <?php 
+                        $chartdata = $comp_model->piechart_cash();
+                        ?>
+                        <div>
+                            <h4>Cash</h4>
+                            <small class="text-muted"></small>
+                        </div>
+                        <hr />
+                        <canvas id="piechart_cash"></canvas>
+                        <script>
+                            $(function (){
+                            var chartData = {
+                            labels : <?php echo json_encode($chartdata['labels']); ?>,
+                            datasets : [
+                            {
+                            label: 'Cash',
+                            backgroundColor:'rgba(0 , 128 , 255, 0.5)',
+                            borderWidth:3,
+                            data : <?php echo json_encode($chartdata['datasets'][0]); ?>,
+                            }
+                            ]
+                            }
+                            var ctx = document.getElementById('piechart_cash');
+                            var chart = new Chart(ctx, {
+                            type:'pie',
+                            data: chartData,
+                            options: {
+                            responsive: true,
+                            scales: {
+                            yAxes: [{
+                            ticks:{display: false},
+                            gridLines:{display: false},
+                            scaleLabel: {
+                            display: true,
+                            labelString: ""
+                            }
+                            }],
+                            xAxes: [{
+                            ticks:{display: false},
+                            gridLines:{display: false},
+                            scaleLabel: {
+                            display: true,
+                            labelString: ""
+                            }
+                            }],
+                            },
+                            }
+                            ,
+                            })});
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ helpers/Menu.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'alamat', 
+			'label' => 'Alamat', 
+			'icon' => '<i class="fa fa-building "></i>'
+		),
+		
+		array(
+			'path' => 'barang', 
+			'label' => 'Barang', 
+			'icon' => '<i class="fa fa-archive "></i>'
+		),
+		
+		array(
+			'path' => 'dompet', 
+			'label' => 'Dompet', 
+			'icon' => '<i class="fa fa-credit-card "></i>'
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => '<i class="fa fa-times-circle "></i>'
+		),
+		
+		array(
+			'path' => 'kas', 
+			'label' => 'Kas', 
+			'icon' => '<i class="fa fa-money "></i>'
+		),
+		
+		array(
+			'path' => 'keranjang', 
+			'label' => 'Keranjang', 
+			'icon' => '<i class="fa fa-shopping-cart "></i>'
+		),
+		
+		array(
+			'path' => 'keuntungan', 
+			'label' => 'Keuntungan', 
+			'icon' => '<i class="fa fa-dollar "></i>'
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => '<i class="fa fa-exchange "></i>'
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => '<i class="fa fa-key "></i>'
+		),
+		
+		array(
+			'path' => 'penjualan', 
+			'label' => 'Penjualan', 
+			'icon' => '<i class="fa fa-line-chart "></i>'
+		),
+		
+		array(
+			'path' => 'perusahaan', 
+			'label' => 'Perusahaan', 
+			'icon' => '<i class="fa fa-institution "></i>'
+		),
+		
+		array(
+			'path' => 'traffics', 
+			'label' => 'Traffics', 
+			'icon' => '<i class="fa fa-tasks "></i>'
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => '<i class="fa fa-male "></i>'
+		)
+	);
+		
+	
+	
+}
